0f19c69e8d27a87d32b08f3acd8a2a85c40432e8
papacheco <papacheco@localhost.localdomain>
2017-03-28 16:12:58 -0300

    urls

--- djangoecommerce/urls.py
+++ djangoecommerce/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from core import views
 
 urlpatterns = [
+    url(r'^$', views.index),
+    url(r'^contato/$', views.contact),
+    url(r'^produto/$', views.product),
+    url(r'^produtos/$', views.product_list),
     url(r'^admin/', admin.site.urls),
 ]
