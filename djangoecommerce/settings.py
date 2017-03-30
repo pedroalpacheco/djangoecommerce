a0fec5bc1eba686e2fe342c90c8461e851d8d3ee
papacheco <papacheco@localhost.localdomain>
2017-03-30 08:48:24 -0300

    removendo comentarios

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
-# Quick-start development settings - unsuitable for production
+# Quick-start development settings - unsuitable for productionn
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
